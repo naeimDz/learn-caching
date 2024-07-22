38c669f1bd79828b91a38d2396a29937f2d1b42d
naeimDz <naeimdz1703@gmail.com>
2024-07-22 21:15:22 +0100

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,35 @@
-module.exports = {
-    async headers() {
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/images/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Customize the output filenames
+    config.output.chunkFilename = `static/${buildId}/[name].js`;
+
+    // Return the updated config
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
+
+
+
+
+  /*async headers() {
       return [
         {
           source: '/images/:all*',
@@ -16,5 +46,4 @@ module.exports = {
       config.output.filename = `static/${buildId}/[name].js`;
       config.output.chunkFilename = `static/${buildId}/[name].js`;
       return config;
-    },
-  };
+    }, */
